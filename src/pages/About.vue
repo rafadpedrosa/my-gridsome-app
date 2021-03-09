50e6a55ff88f20a3d20ae065da9e11986f04ae19
Rafael Pedrosa <rafadpedrosa@gmail.com>
2021-03-09 16:54:15 -0300

    revert me

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -2,6 +2,7 @@
   <Layout>
     <h1>About us</h1>
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error doloremque omnis animi, eligendi magni a voluptatum, vitae, consequuntur rerum illum odit fugit assumenda rem dolores inventore iste reprehenderit maxime! Iusto.</p>
+      <span style="color: white">Eu amo minha namorada</span>
   </Layout>
 </template>
 
